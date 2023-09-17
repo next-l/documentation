10c9ada4e3cb6f6de5d48d4e9294c5054e117c27
Masao Takaku <tmasao@acm.org>
2023-09-17 16:22:20 +0900

    add converter script for asciidoctor-pdf add adoc file for enju_user_all.pdf

---
+++ _pdf/convert-adoc2pdf.rb
@@ -0,0 +1,20 @@
+#!/usr/bin/env ruby
+
+require "tempfile"
+require "fileutils"
+
+if $0 == __FILE__
+  d = File.dirname(__FILE__)
+  files = Dir.glob(File.join(d, "..", "1.4/enju_*.adoc"))
+  FileUtils.cp(files, d)
+  header_regex = /\A---.*?---/m
+  toc_regex = /^include::.*_toc.*$/
+  Dir.glob(File.join(d, "*.adoc")).each do |file|
+    STDERR.puts file
+    cont = open(file).read
+    content_adoc = cont.sub(header_regex, "").strip
+    content_adoc = content_adoc.gsub(toc_regex, "")
+    open(file, "w"){|io| io.print(content_adoc) }
+  end
+  system("asciidoctor-pdf -a pdf-theme=default-with-fallback-font #{File.join(d, "enju_user_all.adoc")}")
+end

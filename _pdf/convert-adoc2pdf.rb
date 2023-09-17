8d18ce54f60f43e63f147a167f0a8c350871d86c
Masao Takaku <tmasao@acm.org>
2023-09-17 21:40:53 +0900

    book type. cover image logo.

--- _pdf/convert-adoc2pdf.rb
+++ _pdf/convert-adoc2pdf.rb
@@ -10,15 +10,18 @@ if $0 == __FILE__
   header_regex = /\A---.*?---/m
   toc_regex = /^include::.*_toc.*$/
   Dir.glob(File.join(d, "*.adoc")).each do |file|
-    STDERR.puts file
+    #STDERR.puts file
     cont = open(file).read
     content_adoc = cont.sub(header_regex, "").strip
     content_adoc = content_adoc.gsub(toc_regex, "")
     open(file, "w"){|io| io.print(content_adoc) }
   end
-  system("asciidoctor-pdf -a pdf-theme=default-with-fallback-font #{File.join(d, "enju_install_vm.adoc")}")
-  system("asciidoctor-pdf -a pdf-theme=default-with-fallback-font #{File.join(d, "enju_setup_all.adoc")}")
-  system("asciidoctor-pdf -a pdf-theme=default-with-fallback-font #{File.join(d, "enju_operation_all.adoc")}")
-  system("asciidoctor-pdf -a pdf-theme=default-with-fallback-font #{File.join(d, "enju_user_all.adoc")}")
-  system("asciidoctor-pdf -a pdf-theme=default-with-fallback-font #{File.join(d, "enju_webapi_all.adoc")}")
+  %w[ enju_install_vm.adoc
+      enju_setup_all.adoc
+      enju_operation_all.adoc
+      enju_user_all.adoc
+      enju_webapi_all.adoc ].each do |file|
+    STDERR.puts file
+    system("asciidoctor-pdf -a pdf-theme=default-with-fallback-font #{File.join(d, file)}")
+  end
 end
